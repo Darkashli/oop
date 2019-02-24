a1c509dca63f2a1efb19d54f2225c77c7c93341f
Alaa Darkashli <darkashli@icloud.com>
2019-02-24 23:46:39 +0100

    Properties + Methods examples
    Creating one class (newclass) where all funtions have included.

---
+++ includes/newclass.inc.php
@@ -0,0 +1,16 @@
+<?php 
+
+  class NewClass {
+    
+    //Properties and Methods goes here
+    public $data = "I am a property";
+
+    public function setNewProperty($newdata) {
+      $this->data = $newdata;
+    }
+
+    public function getProperty() {
+      return $this->data;
+    }
+  }
+
---
+++ includes/parentclass.inc.php
@@ -0,0 +1,7 @@
+<?php
+  class ParentClass{
+
+    //Properties and Methods goes here
+    //Properties: Public, Protected and Private
+    protected $name = "Hey there!";
+  }
